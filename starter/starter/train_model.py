6eec96aa3c5d613fd292f456270525e7dcac7468
李翔 <1516505135@qq.com>
2024-08-25 17:15:23 +0800

    update model train script

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,11 +1,21 @@
-# Script to train machine learning model.
+"""
+Script to train machine learning model
+
+Author: LX
+Date: Aug, 2024
+""" 
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle as pkl
 
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
 
+data = pd.read_csv('../data/clean_census.csv')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -24,5 +34,21 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 # Train and save a model.
+clf_model = train_model(X_train, y_train)
+
+with open('../model/classifier', "wb") as file:
+    pkl.dump([encoder, lb, clf_model], file)
+
+
+train_preds = inference(clf_model, X_train)
+test_preds = inference(clf_model, X_test)
+
+precision, recall, fbeta = compute_model_metrics(y_test, test_preds)
+
+print(f"Precicion: {precision:.2f}")
+print(f"Recall: {recall:.2f}")
+print(f"F1: {fbeta:.2f}")
