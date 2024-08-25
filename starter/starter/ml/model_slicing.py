407cebc4a1a85ab7498fafe9450cf2e1a0cb261f
李翔 <1516505135@qq.com>
2024-08-25 22:12:27 +0800

    bug fix

--- starter/starter/ml/model_slicing.py
+++ starter/starter/ml/model_slicing.py
@@ -9,23 +9,20 @@ from ml.data import process_data
 from ml.model import inference, compute_model_metrics
 
 def slice_metrics(model, encoder, lb, data, target, categorical_features, output_path=None):
-    """Compute metrics by slice of the data.
+    """
     Output the performance of the model on slices of the data
 
-    Inputs
-    ------
-    model: Classifier model (scikit-learn compliant)
-    encoder: trained one-hot-encoder (output of the data.process_data function on training data)
-    lb: trained label binarizer (output of the data.process_data function on training data)
-    df: pandas dataframe where to compute metrics by slice
-    target: target column in the df input dataframe
-    cat_columns: categorical columns
-    output_path: output path to write the output dataframe.
+    Input
+        model: A compatible scikit-learn classifier.
+        encoder: A pre-trained one-hot encoder obtained from the process_data function applied to the training dataset.
+        lb: A pre-trained label binarizer also derived from the process_data function on training data.
+        data: A pandas DataFrame intended for the calculation of metrics across different segments.
+        target: The column within the DataFrame that denotes the target variable.
+        categorical_features:  List of columns identified as categorical types within the DataFrame.
+        output_path: output path to write the output dataframe.
 
-    Returns
-    -------
-    metrics_df: pd.DataFrame
-        Predictions by slice on the input data according the categorical columns.
+    Return
+        metrics_df: pd.DataFrame
     """
     columns = ["column", "category", "precision", "recall", "f1"]
     metrics_df = pd.DataFrame(columns=columns)
